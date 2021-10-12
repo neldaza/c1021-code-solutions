b4fa17f6eaa417b4f95fda483443013eaa6adaa6
Nelson David Daza <yankeenelson@gmail.com>
2021-10-12 13:07:00 -0700

    Javascript Data Model Second Draft

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,79 +1,87 @@
 var orderHistory = [
   {
-    orderNumber: '113-3941689-8772232',
-    dateOrderPlaced: 'August 4, 2020',
-    totalBeforeTax: '$31.55',
-    totalAfterTax: '$34.00',
-    grandTotal: '$34.00',
-    dateDelivered: 'Aug 8, 2020',
-    shipTo: 'JS Masher',
-    returnWindowCloseDate: 'September 7th, 2020',
-    returnWindowClosed: true,
-    packageDelivered: true,
-    handedToResident: true,
-    productCategory: 'books',
-    title: 'Javascript for Impatient programmers',
-    author: 'Rauschmayer, Dr. Axel'
+    orderData: {
+      orderNumber: '113-3941689-8772232',
+      dateOrderPlaced: 'August 4, 2020',
+      grandTotal: '$34.00',
+      dateDelivered: 'Aug 8, 2020',
+      shipTo: 'JS Masher',
+      returnWindowCloseDate: 'September 7th, 2020',
+      returnWindowClosed: true,
+      packageDelivered: true,
+      handedToResident: true
+    },
+    productData: {
+      productCategory: 'books',
+      title: 'Javascript for Impatient programmers',
+      author: 'Rauschmayer, Dr. Axel',
+      itemPriceBeforeTax: '$31.55',
+      itemPriceAfterTax: '$34.00'
+    }
   },
   {
-    orderNumber: '113-9984268-1280257',
-    dateOrderPlaced: 'July 19, 2020',
-    totalBeforeTax: '$41.33',
-    totalAfterTax: '$44.53',
-    grandTotal: '$44.53',
-    dateDelivered: 'Jul 20, 2020',
-    shipTo: 'JS Masher',
-    returnWindowCloseDate: 'Aug 19, 2020',
-    returnWindowClosed: true,
-    packageDelivered: true,
-    handedToResident: true,
-    productCategory: 'books',
-    title: 'The Timeless Way of Building',
-    author: 'Alexander, Christopher'
+    orderData: {
+      orderNumber: '113-9984268-1280257',
+      dateOrderPlaced: 'July 19, 2020',
+      grandTotal: '$44.53',
+      dateDelivered: 'Jul 20, 2020',
+      shipTo: 'JS Masher',
+      returnWindowCloseDate: 'Aug 19, 2020',
+      returnWindowClosed: true,
+      packageDelivered: true,
+      handedToResident: true
+    },
+    productData: {
+      productCategory: 'books',
+      title: 'The Timeless Way of Building',
+      author: 'Alexander, Christopher',
+      itemPriceBeforeTax: '$41.33',
+      itemPriceAfterTax: '$44.53'
+    }
   },
   {
-    orderNumber: '114-2875557-9059409',
-    dateOrderPlaced: 'July 4, 2020',
-    totalBeforeTax: '$15.98',
-    totalAfterTax: '$17.22',
-    grandTotal: '$17.22',
-    dateDelivered: 'Jul 7, 2020',
-    shipTo: 'JS Masher',
-    returnWindowCloseDate: 'Aug 5 2020',
-    returnWindowClosed: true,
-    productDelvered: true,
-    handedToResident: true,
-    productCategory: 'electronics',
-    title: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter'
+    orderData: {
+      orderNumber: '114-2875557-9059409',
+      dateOrderPlaced: 'July 4, 2020',
+      grandTotal: '$17.22',
+      dateDelivered: 'Jul 7, 2020',
+      shipTo: 'JS Masher',
+      returnWindowCloseDate: 'Aug 5 2020',
+      returnWindowClosed: true,
+      productDelvered: true,
+      handedToResident: true
+    },
+    productData: {
+      productCategory: 'electronics',
+      title: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+      itemPriceBeforeTax: '$15.98',
+      itemPriceAfterTax: '$17.22'
+    }
   },
-  [{
-    orderNumber: '113-2883177-2648248',
-    dateOrderPlaced: 'July 3, 2020',
-    totalBeforeTax: '$94.95',
-    totalAfterTax: '$138.93',
-    grandTotal: '$138.93',
-    dateDelivered: 'Jul 5, 2020',
-    shipTo: 'JS Masher',
-    returnWindowCloseDate: 'Aug 4, 2020',
-    returnWindowClosed: true,
-    handedToResident: false,
-    productCategory: 'electronics',
-    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)'
-  },
-
   {
-    orderNumber: '113-2883177-2648248',
-    dateOrderPlaced: 'July 3, 2020',
-    totalBeforeTax: '$33.99',
-    totalAfterTax: '$138.93',
-    grandTotal: '$138.93',
-    dateDelivered: 'Jul 5, 2020',
-    shipTo: 'JS Masher',
-    returnWindowCloseDate: 'Aug 4, 2020',
-    returnWindowClosed: true,
-    handedToResident: false,
-    productCategory: 'books',
-    title: 'The Art of Sql',
-    author: 'Faroult, Stephane'
-  }]
+    orderData: {
+      orderNumber: '113-2883177-2648248',
+      dateOrderPlaced: 'July 3, 2020',
+      totalAfterTax: '$138.93',
+      grandTotal: '$138.93',
+      dateDelivered: 'Jul 5, 2020',
+      shipTo: 'JS Masher',
+      returnWindowCloseDate: 'Aug 4, 2020',
+      returnWindowClosed: true,
+      handedToResident: false
+    },
+    productList: [
+      {
+        productCategory: 'electronics',
+        totalBeforeTax: '$94.95',
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)'
+      },
+      {
+        totalBeforeTax: '$33.99',
+        productCategory: 'books',
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane'
+      }
+    ]
+  }
 ];
