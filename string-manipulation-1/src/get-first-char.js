2404d365edb844152271e61d7f8c9eb1ea5c852d
Nelson David Daza <yankeenelson@gmail.com>
2021-10-16 20:46:34 -0700

    WIP

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,13 @@
+/* exported getFirstChar */
+// 1. Create an action that is named properly for getting the ,
+// last character, and allow an input to be given.
+// 2. looks at current input, and grabs the first value,
+// and assigns this to a new storage
+// 3. returns the value of the new storage value in step 2
+
+function getFirstChar(string) {
+  // eslint-disable-next-line no-unused-vars
+  var word = string;
+  var firstChar = string[0];
+  return firstChar;
+}
---
+++ string-manipulation-1/src/get-last-char.js
@@ -0,0 +1,21 @@
+/* exported getLastChar */
+// 1. define a properly named action for getting the
+// last character of the input and allows an input to be placed
+// 2. create storage for the input
+// 3. access the length of the input and store it
+// in a new variable
+// 4. create an action that accesess the length of the
+// variable, and jumps to the absolutely last value LOCATION
+// and assign this to a new variable (Index)
+// 5. create an action that accesses the last value from the
+// original input with the previous index and assign it to a new
+// variable
+// 6. return the output
+
+function getLastChar(string) {
+  var word = string;
+  var wordLength = word.length;
+  var lastCharIndex = wordLength - 1;
+  var lastChar = string[lastCharIndex];
+  return lastChar;
+}
