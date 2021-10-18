3e44be12f4c149cc5a5ab83d4fef7f415e9cf361
Nelson David Daza <yankeenelson@gmail.com>
2021-10-18 10:41:36 -0700

    WIP

---
+++ javascript-loops-practice/get-indexes.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+/* exported getIndexes */
+function getIndexes(array) {
+  var newArray = [];
+  for (i = 0; i < array.length; i++) {
+    newArray.push(i);
+
+  }
+  return newArray;
+}
