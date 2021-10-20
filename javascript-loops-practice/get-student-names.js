cb12582be2fe9163bf841e276ce99912b1705e3b
Nelson David Daza <yankeenelson@gmail.com>
2021-10-20 15:31:40 -0700

    WIP

--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
 /* exported getStudentNames */
-function getStudentNames(students) {
-  var array = [];
-}
+// 1. define function to pull student names from student object
+// 2. create a for loop that navigates through the entirety of students' length
+// 3. pull the name
