06bdfe272ce85abef95eda1bdf7557f48ac03f72
Nelson David Daza <yankeenelson@gmail.com>
2021-11-01 16:54:27 -0700

    WIP

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,16 @@
 /* exported getWords */
+function getWords(string) {
+  var array = [];
+  var newArray = [];
+  for (var a = 0; a < string.length; a++) {
+    array.push(string);
+  }
+  for (var b = 0; b < array.length; b++) {
+    if (array[b] === '') {
+      array.splice(b);
+    } else {
+      newArray.push(array[b]);
+    }
+  }
+  return newArray;
+}
