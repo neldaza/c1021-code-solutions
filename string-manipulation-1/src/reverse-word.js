46186252f5aeaae72c5ecbb8dce7ce9cfb32f619
Nelson David Daza <yankeenelson@gmail.com>
2021-11-01 15:17:47 -0700

    WIP

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,20 @@
 /* exported reverseWord */
+// 1. define function reverseWord with word perameter
+// . empty string variable
+// 2. assign empty array to variable array
+// 3. loop through entirety of word
+// 4. store indexes of word in array variable
+// 5. loop through array variable and concatenate string variable with reverse indexes of array
+// return string
+
+function reverseWord(word) {
+  var string = '';
+  var array = [];
+  for (var i = 0; i < word.length; i++) {
+    array.push(i);
+  }
+  for (i = 0; i > array.length --i) {
+    string += array[i];
+  }
+  return string;
+}
