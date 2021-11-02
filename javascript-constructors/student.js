fb0266de108935fe7fc6d711bc40a3634e8f8a17
Nelson David Daza <yankeenelson@gmail.com>
2021-11-01 20:28:25 -0700

    Javascript Constructors First Draft

---
+++ javascript-constructors/student.js
@@ -0,0 +1,19 @@
+/* exported Student */
+
+function Student(firstName, lastName, subject) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.subject = subject;
+
+}
+
+Student.prototype.getIntroduction = function () {
+  var introduction = 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
+  return introduction;
+};
+
+Student.prototype.getFullName = function () {
+
+  var fullName = this.firstName + ' ' + this.lastName;
+  return fullName;
+};
