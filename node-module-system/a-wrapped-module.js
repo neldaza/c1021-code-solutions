dc8b782ef46eb605d594b24bc118b59264014741
Nelson <yankeenelson@gmail.com>
2021-11-24 19:29:15 +0000

    Node Module System First Draft

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,5 @@
+console.log('Typeof Exports:', typeof exports, 'Value of exports:', exports);
+console.log('Typeof Require:', typeof require, 'Value of require:', require);
+console.log('Typeof Module:', typeof module, 'Value of Module:', module);
+console.log('Typeof Filename:', typeof __filename, 'Value of Filename', __filename);
+console.log('Typeof Dirname:', typeof __dirname, 'Value of Dirname:', __dirname);
