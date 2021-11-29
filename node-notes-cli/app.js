c94e0e8154772b651d184ab8688081a92ed9a546
Nelson David Daza <yankeenelson@gmail.com>
2021-11-29 15:54:49 -0800

    Feature 2

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,9 +1,24 @@
 const data = require('./data.json');
+const fs = require('fs');
 
-const dataStringify = JSON.stringify(data.notes, null, 2);
+const notesStringify = JSON.stringify(data.notes, null, 2);
+const objectNotes = JSON.parse(notesStringify);
 
-const parsedNotes = JSON.parse(dataStringify);
+const dataStringify = JSON.stringify(data, null, 2);
+const dataObject = JSON.parse(dataStringify);
 
-for (const property in parsedNotes) {
-  console.log(`${property}: ${parsedNotes[property]}`);
+if (process.argv[2] === 'read') {
+  for (const property in objectNotes) {
+    console.log(`${property}: ${objectNotes[property]}`);
+  }
+} else if (process.argv[2] === 'create') {
+  const currentId = dataObject.nextId - 1;
+  dataObject.notes[currentId + 1] = process.argv[3];
+  dataObject.nextId++;
+  const newObject = JSON.stringify(dataObject, null, 2);
+  fs.writeFile('data.json', newObject, 'utf-8', function (err) {
+    if (err) {
+      throw err;
+    }
+  });
 }
