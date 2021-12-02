badfb9b07dcf32a67581d033dccafd76fc19f141
Nelson <yankeenelson@gmail.com>
2021-12-02 05:09:24 +0000

    TASK 2 DONE

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -12,6 +12,30 @@ app.get('/api/notes', function (req, res) {
   res.send(array);
 });
 
-app.listen(3000, () => {
+app.get('/api/notes/:id', function (req, res) {
+  const reqId = Number(req.params.id);
+  if (isNaN(reqId)) {
+    res.sendStatus(400);
+    return;
+  }
+  const array = [];
+  for (const property in data.notes) {
+    if (data.notes[property].id === reqId) {
 
+      array.push(data.notes[property]);
+
+    } else {
+      res.sendStatus(404);
+      return;
+    }
+
+    res.send(array[0]);
+
+  }
+
+});
+
+app.listen(3000, () => {
+// eslint-disable-next-line no-console
+  console.log('The server is listening on port 3000!');
 });
