1d4223f798182142626a6a19d1fad7eb1cce565d
Nelson <yankeenelson@gmail.com>
2021-12-14 04:55:44 +0000

    React Toggle Switch Component First Draft

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import ToggleSwitch from './toggle-switch';
+
+ReactDOM.render(
+  <ToggleSwitch label="Toggle Switch" />,
+  document.getElementById('root')
+);
