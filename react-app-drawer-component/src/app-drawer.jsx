d8353839096650608d36d9cb5ea5ffe2b83713bf
Nelson <yankeenelson@gmail.com>
2021-12-15 18:16:25 +0000

    WIP Skeleton

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClosed: 'yes' };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="column">
+              <i className="tab-icon fas fa-align-justify"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AppDrawer;
