5e233b375a75e6246e80305ec46a7360e9e69c21
Nelson <yankeenelson@gmail.com>
2022-02-07 19:23:55 +0000

    numVowels complete

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,13 @@
 /* exported numVowels */
+function numVowels(string) {
+  let vowelCount = 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  for (let i = 0; i < string.length++; i++) {
+    for (let a = 0; a < vowels.length; a++) {
+      if (string[i] === vowels[a]) {
+        vowelCount++;
+      }
+    }
+  }
+  return vowelCount;
+}
