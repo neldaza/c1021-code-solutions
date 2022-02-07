3ccc5a06a918b06e020ec288d421c2586b5ebd43
Nelson <yankeenelson@gmail.com>
2022-02-07 22:25:50 +0000

    String Manipulation 2 First Draft

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,20 @@
 /* exported swapChars */
+function swapChars(firstIndex, secondIndex, string) {
+  let firstLetter = '';
+  let secondLetter = '';
+  let firstFragment = '';
+  let secondFragment = '';
+  let thirdFragment = '';
+
+  for (let i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      firstFragment = string.slice(0, i);
+      firstLetter = string.slice(i, i + 1);
+    } else if (i === secondIndex) {
+      secondFragment = string.slice(firstIndex + 1, i);
+      secondLetter += string.slice(i, i + 1);
+      thirdFragment = string.slice(secondIndex + 1, string.length);
+    }
+  }
+  return `${firstFragment}${secondLetter}${secondFragment}${firstLetter}${thirdFragment}`;
+}
