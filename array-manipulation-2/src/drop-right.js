e9bef80bfdf2a50b066257c137d0036a99ba62d8
Nelson <yankeenelson@gmail.com>
2022-02-08 01:27:16 +0000

    drop-right.js finished

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,15 @@
 /* exported dropRight */
+function dropRight(array, count) {
+  const newArray = [];
+  const finalArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+
+  const secondArray = newArray.slice(count);
+  for (let a = secondArray.length - 1; a >= 0; a--) {
+    finalArray.push(secondArray[a]);
+  }
+
+  return finalArray;
+}
