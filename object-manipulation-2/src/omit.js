03f6a6cc86e98514be8af0a04232d1283542194d
Nelson <yankeenelson@gmail.com>
2022-02-08 21:39:50 +0000

    Omit Done

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,13 @@
 /* exported omit */
+function omit(source, keys) {
+  const sourceCopy = Object.assign({}, source);
+  for (let i = 0; i < keys.length; i++) {
+    for (const [key] of Object.entries(source)) {
+      if (key === keys[i]) {
+        delete sourceCopy[key];
+      }
+    }
+  }
+
+  return sourceCopy;
+}
