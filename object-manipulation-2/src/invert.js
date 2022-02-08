30694703564742d3cbcfc97b58d367cf849135a5
Nelson <yankeenelson@gmail.com>
2022-02-08 21:50:09 +0000

    invert.js done

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,8 @@
 /* exported invert */
+function invert(source) {
+  const returnObject = {};
+  for (const [key, value] of Object.entries(source)) {
+    returnObject[value] = key;
+  }
+  return returnObject;
+}
