6b1f725a9692105e684688451b7020f4418f6ae1
Nelson <yankeenelson@gmail.com>
2022-02-08 00:32:19 +0000

    take-right.js finished

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,16 @@
 /* exported takeRight */
+function takeRight(array, count) {
+  const newArray = [];
+  const finalArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (newArray.length === count) {
+      for (var a = newArray.length - 1; a >= 0; a--) {
+        finalArray.push(newArray[a]);
+      }
+      return finalArray;
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return [];
+}
