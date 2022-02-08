9113ba8af6179453b0afae8fe46890545be9ef38
Nelson <yankeenelson@gmail.com>
2022-02-08 19:54:22 +0000

    Array Manipulation 2 First Draft

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,24 @@
 /* exported chunk */
+function chunk(array, size) {
+  const arrayCopy = array;
+  let currentChunk = [];
+  const finalChunk = [];
+
+  for (let i = 0; i < arrayCopy.length; i++) {
+    if (currentChunk.length < size) {
+      currentChunk.push(arrayCopy[i]);
+    } else if (i === size) {
+      finalChunk.push(currentChunk);
+      currentChunk = [];
+      currentChunk.push(arrayCopy[i]);
+    } else if (i % size === 0) {
+      finalChunk.push(currentChunk);
+      currentChunk = [];
+      currentChunk.push(arrayCopy[i]);
+    }
+  }
+  if (currentChunk.length) {
+    finalChunk.push(currentChunk);
+  }
+  return finalChunk;
+}
