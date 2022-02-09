0eca49723824073634e6726d15932654ee0cf112
Nelson <yankeenelson@gmail.com>
2022-02-09 17:18:45 +0000

    omit.js for ...in loop used instead of Object.entries()

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -2,12 +2,17 @@
 function omit(source, keys) {
   const sourceCopy = Object.assign({}, source);
   for (let i = 0; i < keys.length; i++) {
-    for (const [key] of Object.entries(source)) {
-      if (key === keys[i]) {
-        delete sourceCopy[key];
+    for (const property in source) {
+      if (property === keys[i]) {
+        delete sourceCopy[property];
       }
     }
-  }
 
+  }
   return sourceCopy;
 }
+
+// for (const [key] of Object.entries(source)) {
+//   if (key === keys[i]) {
+//     delete sourceCopy[key];
+//   }
