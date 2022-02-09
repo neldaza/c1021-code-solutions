a83a34d9d37cac4c305c942f495b2daf7d9ad793
Nelson <yankeenelson@gmail.com>
2022-02-09 01:19:49 +0000

    ANAGRAM WIP

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,18 @@
+/* eslint-disable no-unused-vars */
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  let firstStringNoSpaces = '';
+  let secondStringNoSpaces = '';
+
+  for (let i = 0; i < firstString.length; i++) {
+    if (firstString[i] !== ' ') {
+      firstStringNoSpaces += firstString[i];
+    }
+  }
+  for (let a = 0; a < secondString.length; a++) {
+    if (secondString[a] !== ' ') {
+      secondStringNoSpaces += secondString[a];
+    }
+  }
+
+}
