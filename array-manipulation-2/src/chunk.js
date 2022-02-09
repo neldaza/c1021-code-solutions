3ad0156b94a21c9004b06f644e755f36b901e8bf
Nelson <yankeenelson@gmail.com>
2022-02-09 17:10:11 +0000

    chunk.js incorrect copy variable removed

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,16 +1,15 @@
 /* exported chunk */
 function chunk(array, size) {
-  const arrayCopy = array;
   let currentChunk = [];
   const finalChunk = [];
 
-  for (let i = 0; i < arrayCopy.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (currentChunk.length < size) {
-      currentChunk.push(arrayCopy[i]);
+      currentChunk.push(array[i]);
     } else if (i === size || i % size === 0) {
       finalChunk.push(currentChunk);
       currentChunk = [];
-      currentChunk.push(arrayCopy[i]);
+      currentChunk.push(array[i]);
     }
   }
   if (currentChunk.length) {
