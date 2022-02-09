0b77ff2f5c8190345cf38d4b6d9d566dc060e0d7
Nelson David Daza <yankeenelson@gmail.com>
2022-02-09 15:01:00 -0500

    Initial interval time repaired upon circle click event, old garbage code nixed

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,22 +1,12 @@
-/* eslint-disable no-unused-vars */
-var $circleOne = document.querySelector('.circle-one');
-var $circleTwo = document.querySelector('.circle-two');
-var $circleThree = document.querySelector('.circle-three');
-var $circleFour = document.querySelector('.circle-four');
-var $circleFive = document.querySelector('.circle-five');
 var $img = document.querySelector('img');
-var $rightArrow = document.querySelector('.right');
-var $leftArrow = document.querySelector('.left');
-var $circle = document.querySelector('circle');
-//
-var currentImage = 1;
-var imageSrcArray = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
-var countdown = 4;
-var timerId = null;
 var $circleSelectorAll = document.querySelectorAll('.circle');
 
+var currentImage = 0;
+var imageSrcArray = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
+var timerId = null;
+
 function updatingPicture() {
-  countdown--;
+  currentImage++;
   for (let i = 0; i < imageSrcArray.length; i++) {
     $img.setAttribute('src', imageSrcArray[currentImage]);
     $img.setAttribute('data-image-index', `${currentImage}`);
@@ -32,18 +22,20 @@ function updatingPicture() {
       return;
     }
   }
-  currentImage++;
 }
 
 function circleIndex(event) {
+  clearInterval(timerId);
+  timerId = null;
+  event.target.className = 'circle circle-background';
+  currentImage = parseInt(event.target.getAttribute('data-image-index'));
   $img.setAttribute('src', imageSrcArray[parseInt(event.target.getAttribute('data-image-index'))]);
   for (let i = 0; i < $circleSelectorAll.length; i++) {
     if (parseInt($circleSelectorAll[i].getAttribute('data-image-index')) !== parseInt(event.target.getAttribute('data-image-index'))) {
       $circleSelectorAll[i].className = 'circle';
     }
   }
-  event.target.className = 'circle circle-background';
-  currentImage = parseInt(event.target.getAttribute('data-image-index'));
+  timerId = setInterval(updatingPicture, 3000);
 }
 
 for (let b = 0; b < $circleSelectorAll.length; b++) {
@@ -51,107 +43,3 @@ for (let b = 0; b < $circleSelectorAll.length; b++) {
 }
 
 timerId = setInterval(updatingPicture, 3000);
-
-// function pictureOne(event) {
-//   $img.setAttribute('src', 'images/025.png');
-//   $circleOne.className = 'circle circle-one circle-background';
-//   $circleTwo.className = 'circle circle-two';
-//   $circleThree.className = 'circle circle-three';
-//   $circleFour.className = 'circle circle-four';
-//   $circleFive.className = 'circle circle-five';
-// }
-
-// $circleOne.addEventListener('click', pictureOne);
-
-// function pictureTwo(event) {
-//   $img.setAttribute('src', 'images/039.png');
-//   $circleOne.className = 'circle circle-one';
-//   $circleTwo.className = 'circle circle-two circle-background';
-//   $circleThree.className = 'circle circle-three';
-//   $circleFour.className = 'circle circle-four';
-//   $circleFive.className = 'circle circle-five';
-
-// }
-// $circleTwo.addEventListener('click', pictureTwo);
-
-// function pictureThree(event) {
-//   $img.setAttribute('src', 'images/001.png');
-//   $circleOne.className = 'circle circle-one';
-//   $circleTwo.className = 'circle circle-two';
-//   $circleThree.className = 'circle circle-three circle-background';
-//   $circleFour.className = 'circle circle-four';
-//   $circleFive.className = 'circle circle-five';
-// }
-// $circleThree.addEventListener('click', pictureThree);
-
-// function pictureFour(event) {
-//   $img.setAttribute('src', 'images/004.png');
-//   $circleOne.className = 'circle circle-one';
-//   $circleTwo.className = 'circle circle-two';
-//   $circleThree.className = 'circle circle-three';
-//   $circleFour.className = 'circle circle-four circle-background';
-//   $circleFive.className = 'circle circle-five';
-// }
-// $circleFour.addEventListener('click', pictureFour);
-
-// function pictureFive(event) {
-//   $img.setAttribute('src', 'images/007.png');
-//   $circleOne.className = 'circle circle-one';
-//   $circleTwo.className = 'circle circle-two';
-//   $circleThree.className = 'circle circle-three';
-//   $circleFour.className = 'circle circle-four';
-//   $circleFive.className = 'circle circle-five circle-background';
-// }
-// $circleFive.addEventListener('click', pictureFive);
-
-// function nextPicture(event) {
-//   if ($circleFour.className === 'circle circle-four circle-background') {
-//     pictureFive();
-//   } else if ($circleFive.className === 'circle circle-five circle-background') {
-//     pictureOne();
-//   } else if ($circleOne.className === 'circle circle-one circle-background') {
-//     pictureTwo();
-//   } else if ($circleTwo.className === 'circle circle-two circle-background') {
-//     pictureThree();
-//   } else if ($circleThree.className === 'circle circle-three circle-background') {
-//     pictureFour();
-//   }
-// }
-
-// $rightArrow.addEventListener('click', nextPicture);
-
-// function previousPicture(event) {
-//   if ($circleFour.className === 'circle circle-four circle-background') {
-//     pictureThree();
-//   } else if ($circleFive.className === 'circle circle-five circle-background') {
-//     pictureFour();
-//   } else if ($circleOne.className === 'circle circle-one circle-background') {
-//     pictureFive();
-//   } else if ($circleTwo.className === 'circle circle-two circle-background') {
-//     pictureOne();
-//   } else if ($circleThree.className === 'circle circle-three circle-background') {
-//     pictureTwo();
-//   }
-// }
-
-// $leftArrow.addEventListener('click', previousPicture);
-
-// function updatingPicture() {
-//   countdown--;
-//   if ($circleOne.className === 'circle circle-one circle-background') {
-//     pictureTwo();
-//     countdown = 4;
-//   } else if ($circleTwo.className === 'circle circle-two circle-background') {
-//     pictureThree();
-//     countdown = 3;
-//   } else if ($circleThree.className === 'circle circle-three circle-background') {
-//     pictureFour();
-//     countdown = 2;
-//   } else if ($circleFour.className === 'circle circle-four circle-background') {
-//     pictureFive();
-//     countdown = 1;
-//   } else if ($circleFive.className === 'circle circle-five circle-background') {
-//     pictureOne();
-//     countdown = 0;
-//   }
-// }
