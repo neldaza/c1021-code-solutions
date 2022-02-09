222de736f4f9172b0b8c4baa4763bdde384591c2
Nelson <yankeenelson@gmail.com>
2022-02-09 17:57:09 +0000

    pick.js extra value loop removed

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,10 +3,10 @@ function pick(source, keys) {
   const returnObject = {};
 
   for (let i = 0; i < keys.length; i++) {
-    for (const [key, value] of Object.entries(source)) {
+    for (const key in source) {
       if (key === keys[i]) {
-        if (value !== undefined) {
-          returnObject[keys[i]] = value;
+        if (source[key] !== undefined) {
+          returnObject[keys[i]] = source[key];
         }
       }
     }
