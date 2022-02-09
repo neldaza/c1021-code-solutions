41cb72b54a387ad29d04cf3573bbe41789c7e5f3
Nelson David Daza <yankeenelson@gmail.com>
2022-02-09 15:50:26 -0500

    Javascript Carousel Revised, Cleaned Second Draft

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,5 +1,7 @@
 var $img = document.querySelector('img');
 var $circleSelectorAll = document.querySelectorAll('.circle');
+var $nextArrow = document.querySelector('.next-arrow');
+var $previousArrow = document.querySelector('.previous-arrow');
 
 var currentImage = 0;
 var imageSrcArray = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
@@ -38,8 +40,69 @@ function circleIndex(event) {
   timerId = setInterval(updatingPicture, 3000);
 }
 
+function lastPicture(event) {
+  clearInterval(timerId);
+  if (currentImage === 0) {
+    currentImage = imageSrcArray.length - 1;
+    $img.setAttribute('src', imageSrcArray[currentImage]);
+    $img.setAttribute('data-image-index', currentImage);
+    for (let i = 0; i < $circleSelectorAll.length; i++) {
+      if (parseInt($circleSelectorAll[i].getAttribute('data-image-index')) === parseInt($img.getAttribute('data-image-index'))) {
+        $circleSelectorAll[i].className = 'circle circle-background';
+      } else {
+        $circleSelectorAll[i].className = 'circle';
+      }
+    }
+    timerId = setInterval(updatingPicture, 3000);
+    return;
+  }
+  currentImage = parseInt($img.getAttribute('data-image-index')) - 1;
+  $img.setAttribute('src', imageSrcArray[currentImage]);
+  $img.setAttribute('data-image-index', parseInt($img.getAttribute('data-image-index')) - 1);
+  for (let i = 0; i < $circleSelectorAll.length; i++) {
+    if (parseInt($circleSelectorAll[i].getAttribute('data-image-index')) === parseInt($img.getAttribute('data-image-index'))) {
+      $circleSelectorAll[i].className = 'circle circle-background';
+    } else {
+      $circleSelectorAll[i].className = 'circle';
+    }
+  }
+  timerId = setInterval(updatingPicture, 3000);
+}
+
+function nextPicture(event) {
+  clearInterval(timerId);
+  if (currentImage === imageSrcArray.length - 1) {
+    currentImage = 0;
+    $img.setAttribute('src', imageSrcArray[currentImage]);
+    $img.setAttribute('data-image-index', '0');
+    for (let i = 0; i < $circleSelectorAll.length; i++) {
+      if (parseInt($circleSelectorAll[i].getAttribute('data-image-index')) === parseInt($img.getAttribute('data-image-index'))) {
+        $circleSelectorAll[i].className = 'circle circle-background';
+      } else {
+        $circleSelectorAll[i].className = 'circle';
+      }
+    }
+    timerId = setInterval(updatingPicture, 3000);
+    return;
+  }
+  currentImage = parseInt($img.getAttribute('data-image-index')) + 1;
+  $img.setAttribute('src', imageSrcArray[currentImage]);
+  $img.setAttribute('data-image-index', parseInt($img.getAttribute('data-image-index')) + 1);
+  for (let i = 0; i < $circleSelectorAll.length; i++) {
+    if (parseInt($circleSelectorAll[i].getAttribute('data-image-index')) === parseInt($img.getAttribute('data-image-index'))) {
+      $circleSelectorAll[i].className = 'circle circle-background';
+    } else {
+      $circleSelectorAll[i].className = 'circle';
+    }
+  }
+  timerId = setInterval(updatingPicture, 3000);
+}
+
 for (let b = 0; b < $circleSelectorAll.length; b++) {
   $circleSelectorAll[b].addEventListener('click', circleIndex);
 }
+
+$nextArrow.addEventListener('click', nextPicture);
+$previousArrow.addEventListener('click', lastPicture);
 
 timerId = setInterval(updatingPicture, 3000);
