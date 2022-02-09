b19a659b43566d566f155f1ebb6d56699c0bc9a3
Nelson <yankeenelson@gmail.com>
2022-02-09 16:55:42 +0000

    Drop-right.js no slice()

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,6 +1,6 @@
 /* exported dropRight */
 function dropRight(array, count) {
-  const arrayCopy = array.slice();
+  const arrayCopy = array;
   const newArray = [];
 
   for (let i = 0; i < arrayCopy.length - count; i++) {
