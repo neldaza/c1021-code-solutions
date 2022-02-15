b7b477a553e198a6c2a6135b675486e92f56899c
Nelson David Daza <yankeenelson@gmail.com>
2022-02-15 15:49:57 -0500

    Data Structures Stacks First Draft

--- data-structures-stacks/max-value.js
+++ data-structures-stacks/max-value.js
@@ -1,5 +1,20 @@
 /* exported maxValue */
 
 function maxValue(stack) {
+  const stackArray = Array.from(stack.print());
+  let counter = stackArray.length;
+  const numbersArray = [];
 
+  while (counter > 0) {
+    if (stack.peek() !== undefined) {
+      numbersArray.push(stack.peek());
+    }
+    if (numbersArray[0] === undefined) {
+      return -Infinity;
+    }
+    stack.pop();
+    counter--;
+  }
+
+  return Math.max(...numbersArray);
 }
