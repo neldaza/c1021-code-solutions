2cc4528d843cdf50d9517dda57bcb17de1ca48ae
Nelson <yankeenelson@gmail.com>
2022-02-16 00:17:56 +0000

    2nd from top with no arrays

--- data-structures-stacks/get-2nd-from-top.js
+++ data-structures-stacks/get-2nd-from-top.js
@@ -1,19 +1,24 @@
 /* exported get2ndFromTop */
 
 function get2ndFromTop(stack) {
+  let counter = 0;
+  let firstNumber = null;
+  let secondNumber = null;
 
-  const popArray = [];
-  popArray.push(stack.peek());
-  stack.pop();
-  popArray.push(stack.peek());
-
-  if (popArray.length === 1) {
-    stack.push(popArray[0]);
-    return popArray[0];
-  } else if (popArray[0] === undefined) {
-    return undefined;
-  } else {
-    stack.push(popArray[0]);
-    return popArray[1];
+  while (counter !== 2) {
+    if (counter === 0) {
+      firstNumber = stack.pop();
+      counter++;
+    } else if (counter === 1) {
+      secondNumber = stack.pop();
+      counter++;
+    }
   }
+  if (secondNumber !== undefined) {
+    stack.push(secondNumber);
+  }
+  if (firstNumber !== undefined) {
+    stack.push(firstNumber);
+  }
+  return secondNumber;
 }
