ebdf96b7cc5fbfcb6d9c86dead90b2aa3d54a539
Nelson <yankeenelson@gmail.com>
2022-02-16 19:58:15 +0000

    extra Home component deleted, only images passed cleanly to carousel as props in index.jsx

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 
-export default class Home extends React.Component {
+export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [
-        { photoId: 0, url: 'images/001.png' },
-        { photoId: 1, url: 'images/004.png' },
-        { photoId: 2, url: 'images/007.png' },
-        { photoId: 3, url: 'images/025.png' },
-        { photoId: 4, url: 'images/039.png' }
-      ],
       currentPhoto: 0
     };
     this.circleClick = this.circleClick.bind(this);
     this.arrowClick = this.arrowClick.bind(this);
-
   }
 
   componentDidMount() {
     this.carouselInterval = setInterval(() => {
-      if (this.state.currentPhoto === this.state.images.length - 1) {
+      if (this.state.currentPhoto === this.props.images.length - 1) {
         this.setState({ currentPhoto: -1 });
       }
       this.setState({ currentPhoto: this.state.currentPhoto + 1 });
@@ -38,8 +30,8 @@ export default class Home extends React.Component {
   arrowClick(event) {
     clearInterval(this.carouselInterval);
     if (event.target.getAttribute('arrow') === 'left' && this.state.currentPhoto === 0) {
-      this.setState({ currentPhoto: this.state.images.length - 1 });
-    } else if (event.target.getAttribute('arrow') === 'right' && this.state.currentPhoto === this.state.images.length - 1) {
+      this.setState({ currentPhoto: this.props.images.length - 1 });
+    } else if (event.target.getAttribute('arrow') === 'right' && this.state.currentPhoto === this.props.images.length - 1) {
       this.setState({ currentPhoto: 0 });
 
     } else if (event.target.getAttribute('arrow') === 'right') {
@@ -54,29 +46,15 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <Carousel
-        images={this.state.images}
-        currentPhoto={this.state.currentPhoto}
-        arrowClick={this.arrowClick}
-        circleClick={this.circleClick}
-      />
-    );
-  }
-}
-
-class Carousel extends React.Component {
-
-  render() {
-    return (
   <div>
     <div className='container'>
       <div className="row justify-content-center">
         <div className="column-third flex justify-content-center">
-          <div className="arrow left relative-fifty previous-arrow" arrow={'left'} onClick={this.props.arrowClick}></div>
+          <div className="arrow left relative-fifty previous-arrow" arrow={'left'} onClick={this.arrowClick}></div>
         </div>
         <div className="column-third">
           {
-            this.props.images.filter(photo => photo.photoId === this.props.currentPhoto).map(photo => {
+            this.props.images.filter(photo => photo.photoId === this.state.currentPhoto).map(photo => {
               return (
                 <img key={photo.photoId} src={photo.url} className="background my-container" />
               );
@@ -84,20 +62,20 @@ class Carousel extends React.Component {
           }
         </div>
         <div className="column-one-third flex justify-center">
-          <div className="arrow right relative-fifty next-arrow" arrow={'right'} onClick={this.props.arrowClick}></div>
+          <div className="arrow right relative-fifty next-arrow" arrow={'right'} onClick={this.arrowClick}></div>
         </div>
       </div>
       <div className="row justify-content-center">
         <div className="flex space-evenly">
           {
             this.props.images.map(photo => {
-              if (this.props.currentPhoto === photo.photoId) {
+              if (this.state.currentPhoto === photo.photoId) {
                 return (
-                  <div key={photo.photoId} className="circle circle-background" imageindex={photo.photoId} onClick={this.props.circleClick}></div>
+                  <div key={photo.photoId} className="circle circle-background" imageindex={photo.photoId} onClick={this.circleClick}></div>
                 );
               }
               return (
-                <div key={photo.photoId} className="circle" imageindex={photo.photoId} onClick={this.props.circleClick}></div>
+                <div key={photo.photoId} className="circle" imageindex={photo.photoId} onClick={this.circleClick}></div>
               );
             })
           }
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -1,8 +1,18 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Home from './carousel';
+import Carousel from './carousel';
+
+const carousel = <Carousel
+  images={[
+    { photoId: 0, url: 'images/001.png' },
+    { photoId: 1, url: 'images/004.png' },
+    { photoId: 2, url: 'images/007.png' },
+    { photoId: 3, url: 'images/025.png' },
+    { photoId: 4, url: 'images/039.png' }
+  ]}
+/>;
 
 ReactDOM.render(
-  <Home />,
+  carousel,
   document.getElementById('root')
 );
