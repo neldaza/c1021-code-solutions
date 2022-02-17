459ae16881b27ffa5378de9f2ba9e71bfd7510a8
Nelson <yankeenelson@gmail.com>
2022-02-17 23:41:14 +0000

    Data Structures Queues First Draft

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -2,4 +2,21 @@
 
 function takeNextSmallest(queue) {
 
+  let predecessor = null;
+
+  if (queue.peek() === undefined) {
+    return;
+  }
+
+  while (queue.peek() !== undefined) {
+    predecessor = queue.dequeue();
+
+    if (queue.peek() === undefined) {
+      return predecessor;
+    } else if (predecessor <= queue.peek()) {
+      return predecessor;
+    }
+
+    queue.enqueue(predecessor);
+  }
 }
--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,5 +1,18 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
+  let counter = 0;
+  let cycleValue = null;
 
+  if (queue.peek() === undefined) {
+    return;
+  }
+
+  while (counter !== index) {
+    counter++;
+    cycleValue = queue.dequeue();
+    queue.enqueue(cycleValue);
+  }
+
+  return queue.dequeue();
 }
