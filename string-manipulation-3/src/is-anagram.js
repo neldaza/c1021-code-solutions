8a8f6f11b9dbd52d91528bbb41b227c45ff9c2cc
Nelson <yankeenelson@gmail.com>
2022-02-21 23:16:26 +0000

    is-anagram finished

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,18 +1,37 @@
 /* eslint-disable no-unused-vars */
 /* exported isAnagram */
-function isAnagram(firstString, secondString) {
-  let firstStringNoSpaces = '';
-  let secondStringNoSpaces = '';
 
-  for (let i = 0; i < firstString.length; i++) {
-    if (firstString[i] !== ' ') {
-      firstStringNoSpaces += firstString[i];
-    }
+// Psuedo Code
+
+// first and second strings are stripped of their space values
+
+// The "no space" first and second strings are split into individual letters
+
+// the letters are sorted in ascending order based on their respective UTF-16 Values
+
+// both sorted arrays are called with the join method which returns the arrays as strings
+
+// if the first ordered string is strictly equal to the second ordered string, return true
+
+// else return false
+
+function isAnagram(firstString, secondString) {
+
+  var noSpaceFirstString = firstString.replaceAll(' ', '');
+  var noSpaceSecondString = secondString.replaceAll(' ', '');
+
+  var splitFirstString = noSpaceFirstString.split('');
+  var splitSecondString = noSpaceSecondString.split('');
+
+  var sortFirstString = splitFirstString.sort();
+  var sortSecondString = splitSecondString.sort();
+
+  var orderedFirstString = splitFirstString.join();
+  var orderedSecondString = splitSecondString.join();
+
+  if (orderedFirstString === orderedSecondString) {
+    return true;
   }
-  for (let a = 0; a < secondString.length; a++) {
-    if (secondString[a] !== ' ') {
-      secondStringNoSpaces += secondString[a];
-    }
-  }
+  return false;
 
 }
