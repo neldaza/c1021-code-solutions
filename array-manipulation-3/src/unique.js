34da6db9189141e813ef3fee17bdaccc9674a57d
Nelson <yankeenelson@gmail.com>
2022-02-23 20:19:42 +0000

    unique.js finished

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,20 @@
 /* exported unique */
+
+// Psuedo Code
+
+// create storage for the final array
+// loop through the argument array
+// if array[i] is not present within final array,
+// push array[i] into final array
+
+// return final array
+
+function unique(array) {
+  var finalArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (finalArray.indexOf(array[i]) === -1) {
+      finalArray.push(array[i]);
+    }
+  }
+  return finalArray;
+}
