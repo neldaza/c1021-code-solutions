b4ecf8152192a643df02fdb12ee3de3d2875898f
Nelson <yankeenelson@gmail.com>
2022-02-23 20:32:12 +0000

    Array Manipulation 3 First Draft

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,29 @@
 /* exported union */
+
+// Psuedo Code
+
+// create storage for the final array
+// loop through the first array
+// if final array doesn't have an instance of an element within the first array,
+// add it to final array
+
+// loop through the second array
+// if final array doesnt have an instance of an element within second array
+// add it to final array
+
+// return final array
+
+function union(first, second) {
+  var finalArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (finalArray.indexOf(first[i]) === -1) {
+      finalArray.push(first[i]);
+    }
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (finalArray.indexOf(second[j]) === -1) {
+      finalArray.push(second[j]);
+    }
+  }
+  return finalArray;
+}
