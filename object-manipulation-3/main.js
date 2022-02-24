7987691130cde131ebab7e51849888bcb6310356
Nelson <yankeenelson@gmail.com>
2022-02-24 19:29:34 +0000

    createDeck defined

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -4,3 +4,13 @@ var players = [{ name: 'Nelson', hand: '' }, { name: 'Tim', hand: '' }, { name: 
 var rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
 var suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 var deck = [];
+
+function createDeck() {
+  for (var i = 0; i < rank.length; i++) {
+    for (var j = 0; j < suit.length; j++) {
+      var card = { Rank: rank[i], Suit: suit[j] };
+      deck.push(card);
+    }
+  }
+  return deck;
+}
