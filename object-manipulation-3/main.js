d9dc0f7e5b9310fc4a86129012fb083ace1801d7
Nelson <yankeenelson@gmail.com>
2022-02-24 19:21:11 +0000

    Players created

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,6 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+var players = [{ name: 'Nelson', hand: '' }, { name: 'Tim', hand: '' }, { name: 'Cody', hand: '' }, { name: 'Cassandra', hand: '' }];
+var rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+var suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+var deck = [];
